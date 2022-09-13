f6f181d3fe84383439eb4ba389bad41eb2ab6c97
Georg Dronov <100882767+georgdronov@users.noreply.github.com>
2022-09-13 23:05:32 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    $('.preloader').fadeOut(300);
+
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+});
